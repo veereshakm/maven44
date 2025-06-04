6b39e67fd59000418795829610c899f1bacff21c
veereshakm <kmveeresh283@gmail.com>
2025-06-04 07:31:03 +0530

    first commit

---
+++ myapp/src/main/java/com/mvncmd/example/App.java
@@ -0,0 +1,13 @@
+package com.mvncmd.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
